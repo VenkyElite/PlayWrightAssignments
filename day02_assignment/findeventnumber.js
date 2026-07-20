6607beb429c827b74db8172ce915546367d64cb2
Venkat Pellakuru <77183441+VenkyElite@users.noreply.github.com>
2026-07-20 09:36:09 +0530

    Add files via upload
    day02Assignments

---
+++ day02_assignment/findeventnumber.js
@@ -0,0 +1,6 @@
+let i=1
+console.log("Even Numbers between 1 to 30")
+for (i; i<30; i++)
+    if (i % 2==0) {//Even number is divisible by 2 
+        console.log('The inputNumber '  +i+  ' is Even Number');
+    }
---
+++ day02_assignment/findoddnumber.js
@@ -0,0 +1,6 @@
+let i=1
+console.log("Odd Numbers between 1 to 30")
+for (i; i<30; i++)
+    if (i % 2!=0) {//Odd number cannot  be divisible by 2 and always have reminder value
+        console.log('The input number '  +i+  ' is Odd Number');
+    }
